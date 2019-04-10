f42f8c41bc69f37216a0dcd9d4a944cd7d593f99
hishinuma616 <hishinuma.yuta@a-tm.co.jp>
2019-04-10 15:17:34 +0900

    複数ページ対応

--- Phase1/index.js
+++ Phase1/index.js
@@ -3,9 +3,9 @@ const puppeteer = require('puppeteer')
 const pageList = require('./pageList.js')
 
 async function getScreenShot (url) {
-  const domain = await getDomain(url)
-  const savePath = await getSavePath(domain)
-
+  const fileData = await getDomain(url)
+  await getSavePath(fileData.path)
+  console.log(fileData)
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(url);
@@ -13,21 +13,34 @@ async function getScreenShot (url) {
           width: 1280,
           height: 1000
         });
-  await page.screenshot({ path: domain +'/'+ domain +'.png', fullPage: true })
+  await page.screenshot({ path: fileData.path + fileData.name + '.png', fullPage: true })
   await browser.close();
+  console.log(fileData)
 
   return new Promise((resolve) => {
-    resolve("Done!")
+    resolve('Done!')
   })
 }
 
 async function getDomain(url) {
-  return url.match(/^https?:\/{2,}(.*?)(?:\/|\?|#|$)/)[1]
+  //return url.match(/^https?:\/{2,}(.*?)(?:\/|\?|#|$)/)[1]
+  const data = url.split('/')
+  const fileName = data.slice(-2)[0]
+  data.shift()
+  data.shift()
+  data.pop()
+  data.pop()
+  const filePath = data.reduce((a, b) => a + b +'/', '')
+  const fileData = {
+    name :  fileName,
+    path : filePath
+  }
+  return fileData
 }
 
-async function getSavePath(domain) {
-  if (!fs.existsSync(domain)) {
-    fs.mkdirSync(domain);
+async function getSavePath(path) {
+  if (!fs.existsSync(path)) {
+    fs.mkdirSync(path);
   }
   return
 }
@@ -35,6 +48,8 @@ async function getSavePath(domain) {
 
 pageList.item.forEach((url, index) => {
   getScreenShot(url).then((result) => {
-    console.log(result)
+    console.log(result +':'+ url)
+  }).catch((e)=> {
+    console.log('Error!:'+ e)
   })
 })
--- Phase1/pageList.js
+++ Phase1/pageList.js
@@ -1,6 +1,7 @@
 module.exports = {
   item : [
     'https://internet-connect.net/docomo/',
-    'https://internet-connect.net/au/'
+    'https://internet-connect.net/au/',
+    'https://internet-connect.net/docomo/OCN/'
   ]
 }
